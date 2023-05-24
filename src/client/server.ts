59250f119a4854a31bb832be73fa7b22184a61ee
Gus Narea <gnarea@users.noreply.github.com>
2023-05-24 10:40:42 +0100

    undo more noise

--- src/client/server.ts
+++ src/client/server.ts
@@ -5,7 +5,6 @@ import type { BaseLogger } from 'pino';
 import { makeFastify } from '../utilities/fastify/server.js';
 import { HTTP_STATUS_CODES } from '../utilities/http.js';
 import type { PluginDone } from '../utilities/fastify/PluginDone.js';
-import { ConfigItem } from '../models/ConfigItem.model.js';
 import { ConfigItem2 } from '../models/ConfigItem2.model.js';
 
 function makePohttpClientPlugin(
@@ -19,36 +18,12 @@ function makePohttpClientPlugin(
     server.getDefaultJsonParser('ignore', 'ignore'),
   );
 
-  server.get('/', async (_request, reply) => {
-    await reply.status(HTTP_STATUS_CODES.OK).send('It works');
-  });
-
   server.post('/test1', async (_request, reply) => {
     const privateEndpointModel = getModelForClass(ConfigItem2, {
       existingConnection: server.mongoose,
     });
 
     await privateEndpointModel.findOne({});
-
-    return reply.status(HTTP_STATUS_CODES.ACCEPTED).send();
-  });
-
-  server.post('/test2', async (_request, reply) => {
-    const configItem2Model = getModelForClass(ConfigItem2, {
-      existingConnection: server.mongoose,
-    });
-
-    await configItem2Model.findOne({});
-
-    return reply.status(HTTP_STATUS_CODES.ACCEPTED).send();
-  });
-
-  server.post('/test3', async (_request, reply) => {
-    const configItemModel = getModelForClass(ConfigItem, {
-      existingConnection: server.mongoose,
-    });
-
-    await configItemModel.findOne({});
 
     return reply.status(HTTP_STATUS_CODES.ACCEPTED).send();
   });
