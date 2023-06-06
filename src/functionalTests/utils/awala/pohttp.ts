9fa4648389c2e28d72ce54b56345e0d21600b043
Gus Narea <gnarea@users.noreply.github.com>
2023-06-06 16:53:28 +0100

    PoHTTP client

--- src/functionalTests/utils/awala/pohttp.ts
+++ src/functionalTests/utils/awala/pohttp.ts
@@ -1,12 +1,12 @@
 import { NodeConnectionParams } from '@relaycorp/relaynet-core';
+import { type CloudEvent, HTTP } from 'cloudevents';
 
 import { getServiceUrl } from '../knative.js';
 
 const POHTTP_SERVER_URL = await getServiceUrl('awala-endpoint-pohttp-server');
+const POHTTP_CLIENT_URL = await getServiceUrl('awala-endpoint-pohttp-client');
 
 const CONNECTION_PARAMS_URL = `${POHTTP_SERVER_URL}/connection-params.der`;
-
-const ENDPOINT_URL = POHTTP_SERVER_URL;
 
 const PARCEL_DELIVERY_HEADERS = new Headers();
 PARCEL_DELIVERY_HEADERS.set('Content-Type', 'application/vnd.awala.parcel');
@@ -20,9 +20,19 @@ export async function getConnectionParams(): Promise<NodeConnectionParams> {
 }
 
 export async function postParcel(expiredParcelSerialised: ArrayBuffer) {
-  return fetch(ENDPOINT_URL, {
+  return fetch(POHTTP_SERVER_URL, {
     method: 'POST',
     body: expiredParcelSerialised,
     headers: PARCEL_DELIVERY_HEADERS,
+  });
+}
+
+export async function postEventToPohttpClient(event: CloudEvent<unknown>): Promise<Response> {
+  const message = HTTP.binary(event);
+
+  return fetch(POHTTP_CLIENT_URL, {
+    method: 'POST',
+    headers: message.headers as HeadersInit,
+    body: message.body as string,
   });
 }
