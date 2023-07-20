356ba804e6c5aaa285be0f525eadb566a86a41da
Gus Narea <gnarea@users.noreply.github.com>
2023-07-20 21:33:33 +0100

    feat: Support specifying DB options in separate env vars (#93)
    * feat: Support specifying DB options in separate env vars
    
    MONGODB_DB, MONGODB_USER and MONGODB_PASSWORD.
    
    * add missing tests
    
    * empty
    
    * omit undefined options to avoid auth errors
    
    * omit undefined options altogether to avoid auth errors

--- src/utilities/mongo.ts
+++ src/utilities/mongo.ts
@@ -1,8 +1,17 @@
 import envVar from 'env-var';
-import { type Connection, createConnection } from 'mongoose';
+import { type Connection, type ConnectOptions, createConnection } from 'mongoose';
+
+function omitUndefinedOptions(initialOptions: ConnectOptions): ConnectOptions | undefined {
+  const entries = Object.entries(initialOptions).filter(([, value]) => value !== undefined);
+  const finalOptions = Object.fromEntries(entries);
+  return Object.keys(finalOptions).length === 0 ? undefined : finalOptions;
+}
 
 export function createMongooseConnectionFromEnv(): Connection {
   const mongoUri = envVar.get('MONGODB_URI').required().asString();
-
-  return createConnection(mongoUri);
+  const dbName = envVar.get('MONGODB_DB').asString();
+  const user = envVar.get('MONGODB_USER').asString();
+  const pass = envVar.get('MONGODB_PASSWORD').asString();
+  const options = omitUndefinedOptions({ dbName, user, pass });
+  return createConnection(mongoUri, options);
 }
